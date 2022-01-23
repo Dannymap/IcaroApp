a279d986854b1581f10568ed5b146f57a88b55c2
Dannymap <54427656+Dannymap@users.noreply.github.com>
2022-01-23 02:04:09 -0300

    build the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,10 +9,180 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
+  String _emailError = '';
+  bool _emailShowError = false;
+
+  String _password = '';
+  String _passwordError = '';
+  bool _passwordShowError = false;
+
+  bool _rememberme = true;
+  bool _passwordShow = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login Screen')),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _showLogo(),
+          SizedBox(
+            height: 5,
+          ),
+          _showEmail(),
+          _showPassword(),
+          _showRememberme(),
+          _showButtons(),
+        ],
+      )),
     );
+  }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/LogoCirculo.png'),
+      width: 250,
+    );
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+          autofocus: true,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'Ingresa tu email...',
+            labelText: 'Email',
+            errorText: _emailShowError ? _emailError : null,
+            prefixIcon: Icon(Icons.alternate_email),
+            suffixIcon: Icon(Icons.email),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+          onChanged: (value) {
+            _email = value;
+            // print(_email);
+          }),
+    );
+  }
+
+  Widget _showPassword() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+          obscureText: !_passwordShow,
+          decoration: InputDecoration(
+            hintText: 'Ingresa tu contraseña...',
+            labelText: 'Contraseña',
+            errorText: _passwordShowError ? _passwordError : null,
+            prefixIcon: Icon(Icons.lock),
+            suffixIcon: IconButton(
+              icon: _passwordShow
+                  ? Icon(Icons.visibility)
+                  : Icon(Icons.visibility_off),
+              onPressed: () {
+                setState(() {
+                  _passwordShow = !_passwordShow;
+                });
+              },
+            ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+          onChanged: (value) {
+            _password = value;
+            print(_email);
+          }),
+    );
+  }
+
+  Widget _showRememberme() {
+    return CheckboxListTile(
+      title: Text('Recordarme'),
+      value: _rememberme,
+      onChanged: (value) {
+        setState(() {
+          _rememberme = value!;
+        });
+      },
+    );
+  }
+
+  Widget _showButtons() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Iniciar Sesión'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                  return Color(0xFF120E43);
+                }),
+              ),
+              onPressed: () => _login(),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Registrar Usuario'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                  return Color(0xFFE03B8B);
+                }),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _login() {
+    if (!_validateFields()) {
+      return;
+    }
+  }
+
+  bool _validateFields() {
+    bool hasErrors = false;
+
+    //Valido el campo email
+    if (_email.isEmpty) {
+      hasErrors = true;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar un email.';
+    } else if (!EmailValidator.validate(_email)) {
+      hasErrors = true;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar un email válido';
+    } else {
+      _emailShowError = false;
+    }
+
+    //Valido el campo password
+    if (_password.isEmpty) {
+      hasErrors = true;
+      _passwordShowError = true;
+      _passwordError = 'Debes ingresar una contraseña.';
+    } else if (_password.length < 6) {
+      hasErrors = true;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar una contraña de 6 carácteres minimo.';
+    } else {
+      _passwordShowError = false;
+    }
+
+    setState(() {});
+    return hasErrors;
   }
 }
